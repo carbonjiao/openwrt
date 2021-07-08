78dbf70ffc11c14d23bbf6611fabc3ffb7bde727
carbonjiao <carbonjiao@gmail.com>
2021-07-08 23:37:24 +0800

    Update diy-part1.sh

--- .github/workflows/diy-part1.sh
+++ .github/workflows/diy-part1.sh
@@ -12,8 +12,8 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git custom https://github.com/kenzok8/jell.git' feeds.conf.default
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+#sed -i '$a src-git custom https://github.com/kenzok8/jell.git' feeds.conf.default
+#sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 
 # version
 TZ='Asia/Shanghai' echo "r`date '+%y%m%d'`-`git log -n 1 --format='%H'|cut -c-8`" > version
