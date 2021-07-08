941ad5f5ea2cdd5a1ce276c0a13f49caa08b261d
carbonjiao <carbonjiao@gmail.com>
2021-07-08 20:10:07 +0800

    Update diy-part1.sh

--- .github/workflows/diy-part1.sh
+++ .github/workflows/diy-part1.sh
@@ -12,7 +12,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git custom https://github.com/kenzok8/jell.git' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 
 # version
